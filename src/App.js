ca2825a9d2641c34c209a62d620bd465f991e086
Mahir Asief <asiefmahir1@gmail.com>
2023-06-05 15:07:10 +0600

    Learn about how to write react components and how to make those reusable using props

--- src/App.js
+++ src/App.js
@@ -6,7 +6,26 @@ function App() {
   
   return (
     <div className="App">
-        Hello World
+        <BioData 
+            bgColor="green"
+            name="Mahir Asief"
+            age={27}
+            phone = "01565746765876587658768"
+            email = "asiefmahir1@gmail.com"
+            skills = {["Js", "React", "Node", "MongoDB"]}
+            socialLinks={["fb/asiefmahir", 'li/asiefmahir', "tw/asiefmahir"]}
+            interests={["Ds Algo", "System Design"]}
+        />
+        <hr />
+        <BioData 
+            name="Mahadi Hasan"
+            age={25}
+            phone = "0145676845"
+            email = "mahadi@gmail.com"
+            skills = {["Js", "React", "Node", "MongoDB", "bootstrap", "css"]}
+            socialLinks={["fb/mahadi", 'li/mahadi', "tw/mahadi"]}
+            interests={["Aws", "ML", "AI"]}
+        />
     </div>
   );
 }
