8068f3be904c9546d084763757535f8f37110e39
Mahir Asief <asiefmahir1@gmail.com>
2023-06-24 22:03:00 +0600

    Learn about how to connect the backend apis from our react app for all the functionalities

--- src/App.js
+++ src/App.js
@@ -5,38 +5,93 @@ import "./App.css";
 import { useState, useEffect } from "react";
 
 const App = () => {
-	// const [isLoading, setIsLoading] = useState(true)
-	const [data, setData] = useState([]);
-	const [isLoading, setIsLoading] = useState(true)
-	const [errorMessage, setErrorMessage] = useState('')
-	const [counter, setCounter] = useState(10);
-	const [counter2, setCounter2] = useState(50);
+	const [noteTitle, setNoteTitle] = useState("");
+	const [editMode, setEditMode] = useState(false);
+	const [editableNote, setEditableNote] = useState(null);
+	const [notes, setNotes] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [errorMessage, setErrorMessage] = useState("");
 
-	const increaseHandler = () => {
-		setCounter(counter + 1);
+	const getAllNotes = () => {
+		fetch("http://localhost:4000/notes")
+			.then((res) => {
+				setIsLoading(true);
+				return res.json();
+			})
+			.then((result) => {
+				setIsLoading(false);
+				setNotes(result);
+				setErrorMessage("");
+			})
+			.catch((err) => {
+				setIsLoading(false);
+				setErrorMessage(err.message);
+				setNotes([]);
+			});
 	};
 
-	const increaseHandler2 = () => {
-		setCounter2(counter2 + 5);
+	const createHandler = () => {
+		if (!noteTitle) {
+			return alert(`Please provide a valid title`);
+		}
+
+		const newNote = {
+			id: Date.now() + "",
+			title: noteTitle,
+		};
+
+		fetch(`http://localhost:4000/notes`, {
+			method: "POST",
+			headers: {
+				"Content-type": "application/json",
+			},
+			body: JSON.stringify(newNote),
+		}).then(() => {
+			getAllNotes();
+			setNoteTitle('')
+		});
 	};
+
+	const removeHandler = (id) => {
+		fetch(`http://localhost:4000/notes/${id}`, {
+			method: "DELETE"
+		})
+			.then(() => {
+				getAllNotes()
+			})
+	}
+
+	const editHandler = (id) => {
+		const toBeUpdatedNote = notes.find(item => item.id === id);
+		setEditMode(true);
+		setEditableNote(toBeUpdatedNote);
+		setNoteTitle(toBeUpdatedNote.title)
+	}
+
+
+	const updateHandler = () => {
+		if (!noteTitle) {
+			return alert(`Please provide a valid title`);
+		}
+
+		fetch(`http://localhost:4000/notes/${editableNote.id}`, {
+			method: "PUT",
+			headers: {
+				'Content-type': 'application/json'
+			},
+			body: JSON.stringify({id: editableNote.id, title: noteTitle})
+		})
+			.then(() => {
+				getAllNotes();
+				setNoteTitle('');
+				setEditMode(false);
+				setEditableNote(null)
+			})
+	}
 
 	useEffect(() => {
 		console.log("I am being called");
-		fetch("https://jsonplaceholder.typicode.com/posts")
-			.then((res) => {
-				setIsLoading(true)
-				return res.json()
-			})
-			.then((result) => {
-				setIsLoading(false)
-				setData(result)
-				setErrorMessage('')
-			})
-			.catch(err => {
-				setIsLoading(false)
-				setErrorMessage(err.message)
-				setData([])
-			})
+		getAllNotes()
 	}, []);
 
 	console.log("App rerendering");
@@ -44,15 +99,30 @@ const App = () => {
 	return (
 		<div className="App">
 			<h2>API Calling in React</h2>
-			<p>The value of the counter is {counter}</p>
-			<button onClick={increaseHandler}>Increase By 1</button>
-			<p>The value of the counter2 is {counter2}</p>
-			<button onClick={increaseHandler2}>Increase By 1</button>
+			<form
+				onSubmit={(e) => {
+					e.preventDefault();
+					editMode ? updateHandler() :  createHandler()
+				}}
+			>
+				<input
+					type="text"
+					value={noteTitle}
+					onChange={(e) => setNoteTitle(e.target.value)}
+				/>
+				<button type="submit">
+					{editMode ? "Update Note" : "Create Note"}
+				</button>
+			</form>
 			{isLoading && <div>Loading/......</div>}
 			{errorMessage && <h2>{errorMessage}</h2>}
 			<ul>
-				{data.map((item) => (
-					<li key={item.id}>{item.title}</li>
+				{notes.map((item) => (
+					<li key={item.id}>
+						<span>{item.title}</span>
+						<button onClick={() => editHandler(item.id)}>Edit</button>
+						<button onClick={() => removeHandler(item.id)}>Remove</button>
+					</li>
 				))}
 			</ul>
 		</div>
