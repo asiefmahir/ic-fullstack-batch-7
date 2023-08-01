b82cd024b9660f71ed3b15603e2eb9d1406921cd
Asief Mahir <asiefmahir1@gmail.com>
2023-07-31 22:41:24 -0700

    FEAT: Learn about createAsyncThuk function in Redux Toolkit and The Fundamentals of rtk query

--- src/store/index.js
+++ src/store/index.js
@@ -1,35 +1,43 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import {composeWithDevTools} from '@redux-devtools/extension'
+// import { createStore, combineReducers, applyMiddleware } from "redux";
+// import {composeWithDevTools} from '@redux-devtools/extension'
+// import { counterReducer } from "./reducers/counter";
+// import { themeReducer } from "./reducers/theme";
+// import { cartReducer } from "./reducers/cart";
+// // import { ourMiddleWare } from "./middlewares";
+// import logger from 'redux-logger'
+// import { postReducer } from "./reducers/post";
+// import { todoReducer } from "./reducers/todo";
+// import thunk from 'redux-thunk'
+
+// const rootReducer = combineReducers({
+//     counter: counterReducer,
+//     theme: themeReducer,
+//     cart: cartReducer,
+//     post: postReducer,
+//     todo: todoReducer
+// })
+
+// export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger, thunk)))
+
+import {configureStore, combineReducers} from '@reduxjs/toolkit';
 import { counterReducer } from "./reducers/counter";
 import { themeReducer } from "./reducers/theme";
-import { cartReducer } from "./reducers/cart";
-// import { ourMiddleWare } from "./middlewares";
-import logger from 'redux-logger'
+import {cartReducer} from "./reducers/cart";
 import { postReducer } from "./reducers/post";
-import { todoReducer } from "./reducers/todo";
-import thunk from 'redux-thunk'
+// import todoReducer from './reducers/todo';
+import { todoApi } from './reducers/todo';
+
 
 const rootReducer = combineReducers({
     counter: counterReducer,
     theme: themeReducer,
     cart: cartReducer,
     post: postReducer,
-    todo: todoReducer
+    [todoApi.reducerPath]: todoApi.reducer
+    
 })
 
-export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger, thunk)))
-
-// import {configureStore, combineReducers} from '@reduxjs/toolkit';
-// import { counterReducer } from "./reducers/counter";
-// import { themeReducer } from "./reducers/theme";
-// import {cartReducer} from "./reducers/cart";
-
-// const rootReducer = combineReducers({
-//     counter: counterReducer,
-//     theme: themeReducer,
-//     cart: cartReducer
-// })
-
-// export const store = configureStore({
-//     reducer: rootReducer,
-// })
+export const store = configureStore({
+    reducer: rootReducer,
+    middleware: (gdm) => gdm().concat(todoApi.middleware)
+})
